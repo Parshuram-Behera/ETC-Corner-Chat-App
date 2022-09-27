e4ffc5252c56cb4abf4923421ef2b3ddeea27b3e
Parshuramb7 <parshurambehera7735@gmail.com>
2022-09-27 17:54:38 +0530

    Profile Setup Activity All Work Done

--- app/src/main/java/com/example/test/ProfileActivity.java
+++ app/src/main/java/com/example/test/ProfileActivity.java
@@ -1,14 +1,19 @@
 package com.example.test;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 
 public class ProfileActivity extends AppCompatActivity {
 
+    ImageView select_img ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +24,31 @@ public class ProfileActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
 
+        select_img = findViewById(R.id.profile_image);
+
+        select_img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent , 45);
+
+            }
+        });
 
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if ( data != null){
+
+            if ( data.getData() != null){
+                select_img.setImageURI(data.getData());
+            }
+        }
     }
 }
