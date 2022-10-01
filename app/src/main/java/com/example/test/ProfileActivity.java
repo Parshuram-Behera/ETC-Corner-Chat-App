24bc1df63a19878262003bdc438646a263b20041
Parshuramb7 <parshurambehera7735@gmail.com>
2022-10-01 18:52:26 +0530

    new changes

--- app/src/main/java/com/example/test/ProfileActivity.java
+++ app/src/main/java/com/example/test/ProfileActivity.java
@@ -8,12 +8,18 @@ import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class ProfileActivity extends AppCompatActivity {
 
     ImageView select_img ;
+    TextView btn_next3;
+    EditText user_name;
+    EditText user_about;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +32,19 @@ public class ProfileActivity extends AppCompatActivity {
 
         select_img = findViewById(R.id.profile_image);
 
+        btn_next3 = findViewById(R.id.btn_next3);
+
+        user_name = findViewById(R.id.User_name);
+
+        user_about = findViewById(R.id.User_about);
+
+
+
+
+
+
+
+
         select_img.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,6 +53,24 @@ public class ProfileActivity extends AppCompatActivity {
                 intent.setAction(Intent.ACTION_GET_CONTENT);
                 intent.setType("image/*");
                 startActivityForResult(intent , 45);
+
+            }
+        });
+
+
+        btn_next3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (user_name.getText().toString().trim().isEmpty()){
+                    Toast.makeText(ProfileActivity.this, "Please Set Your Name", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Intent intent = new Intent(ProfileActivity.this ,UserInterActionActivity.class);
+                    intent.putExtra("UserAbout" ,user_about.getText().toString().trim());
+                    startActivity(intent);
+
+                }
 
             }
         });
