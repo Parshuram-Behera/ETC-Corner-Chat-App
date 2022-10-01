d1f0afa2a5ea815ee07ba28e8f42f652551f6a7c
Parshuramb7 <parshurambehera7735@gmail.com>
2022-10-01 19:48:28 +0530

    new changes

---
+++ app/src/main/java/com/example/test/Modals/Userdata.java
@@ -0,0 +1,32 @@
+package com.example.test.Modals;
+
+public class Userdata {
+
+    private String UID, Username ;
+
+    public Userdata(String UID, String username) {
+        this.UID = UID;
+        Username = username;
+    }
+
+    public String getUID() {
+        return UID;
+    }
+
+    public void setUID(String UID) {
+        this.UID = UID;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+
+
+
+
+}
